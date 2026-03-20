f587a96f47295203534b01aca86d8ecf0a99027b
Hamster5295 <hamster5295@163.com>
2026-03-20 17:12:21 +0800

    feat: Tile.map

--- chisel/Tile.scala
+++ chisel/Tile.scala
@@ -7,48 +7,61 @@ import chisel3.util._
 
 class TileSeq[T](val xLen: Int, val yLen: Int)(gen: (Int, Int) => T) {
   val inner: Seq[Seq[T]] = Seq.tabulate(xLen)(x =>
-    Seq.tabulate(yLen)(y => prefix(s"pos_${x}_$y")(gen(x, y))),
+    Seq.tabulate(yLen)(y => prefix(s"pos_${x}_$y")(gen(x, y)))
   )
 
   def apply(x: Int, y: Int) = inner(x)(y)
-  def x(idx:   Int)         = inner(if (idx >= 0) idx else xLen - idx)
-  def y(idx: Int) = inner.map(i => i(if (idx >= 0) idx else yLen + idx))
-  def foreach(f: (T, Int, Int) => Unit): Unit = inner.zipWithIndex
-    .flatMap { case (seq, row) =>
-      seq.zipWithIndex.map { case (el, col) => (el, row, col) }
-    }.map { case (el, row, col) => f(el, row, col) }
+  def x(idx:   Int) = inner(if (idx >= 0) idx else xLen - idx)
+  def y(idx:   Int) = inner.map(i => i(if (idx >= 0) idx else yLen + idx))
+  def map[B](f: (T, Int, Int) => B): TileSeq[B] =
+    TileSeq(xLen, yLen, (x, y) => f(apply(x, y), x, y))
 }
 
 object TileSeq {
-  def apply[T](xLen: Int, yLen: Int)(gen: (Int, Int) => T): TileSeq[T] =
+  def apply[T](xLen: Int, yLen: Int, gen: (Int, Int) => T): TileSeq[T] =
     new TileSeq(xLen, yLen)(gen)
+
+  def apply[T](xLen: Int, yLen: Int, gen: => T): TileSeq[T] =
+    new TileSeq(xLen, yLen)((_, _) => gen)
+
 }
 
-class Tile[T <: Data](xLen: Int, yLen: Int)(gen: => T, isType: Boolean)
+class Tile[T <: Data](xLen: Int, yLen: Int)(gen: (Int, Int) => T, isType: Boolean)
     extends Bundle {
   val bits = if (isType)
-    Vec(xLen, Vec(yLen, gen))
+    Vec(xLen, Vec(yLen, gen(0, 0)))
   else VecInit(Seq.tabulate(xLen)(x =>
-    VecInit(Seq.tabulate(yLen)(y => prefix(s"pos_${x}_$y")(gen))),
+    VecInit(Seq.tabulate(yLen)(y => prefix(s"pos_${x}_$y")(gen(x, y))))
   ))
 
-  def apply(x: Int, y: Int) = bits(x)(y)
-  def x(idx:   Int)         = bits(if (idx >= 0) idx else xLen - idx)
-  def y(idx: Int) = VecInit(bits.map(i => i(if (idx >= 0) idx else yLen + idx)))
+  def apply(x: Int, y: Int): T      = bits(x)(y)
+  def x(idx:   Int):         Vec[T] = bits(if (idx >= 0) idx else xLen - idx)
+  def y(idx:   Int):         Vec[T] = VecInit(bits.map(i => i(if (idx >= 0) idx else yLen + idx)))
+  def map[B <: Data](f: (T, Int, Int) => B): Tile[B] =
+    TileInit(xLen, yLen, (x, y) => f(apply(x, y), x, y))
 }
 
 object Tile {
   def apply[T <: Data](
       xLen: Int,
       yLen: Int,
-  )(gen: => T) =
-    new Tile(xLen, yLen)(gen, true)
+      gen:  => T
+  ) =
+    new Tile(xLen, yLen)((_, _) => gen, true)
 }
 
 object TileInit {
   def apply[T <: Data](
       xLen: Int,
       yLen: Int,
-  )(gen: => T) =
+      gen:  => T
+  ) =
+    new Tile(xLen, yLen)((_, _) => gen, false)
+
+  def apply[T <: Data](
+      xLen: Int,
+      yLen: Int,
+      gen:  (Int, Int) => T
+  ) =
     new Tile(xLen, yLen)(gen, false)
 }
