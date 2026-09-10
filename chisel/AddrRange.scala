f59d5efe132a546a04c7e66560641af567c1ac15
Hamster5295 <hamster5295@163.com>
2026-09-10 14:43:57 +0800

    feat: AddrRange

---
+++ chisel/AddrRange.scala
@@ -0,0 +1,27 @@
+package hammer
+
+import chisel3._
+import chisel3.util._
+
+case class AddrRange(start: Int, length: Int) {
+  def end                  = start + length
+  def isInside(addr: UInt) = addr >= start.U && addr < end.U
+
+  override def toString() = s"[$start, ${start + length})"
+}
+
+object AddrRange {
+
+  def getFirstOverlap(addrs: Seq[AddrRange]): Option[(AddrRange, AddrRange)] = {
+    // We first sort the address ranges by starting address, then compare them one by one
+    val sorted = addrs.sortBy(_.start)
+    for (i <- 0 until (sorted.length - 1)) {
+      if (sorted(i).end > sorted(i + 1).start) {
+        return Some(sorted(i), sorted(i + 1))
+      }
+    }
+    return None
+  }
+
+  def isOverlap(addrs: Seq[AddrRange]): Boolean = getFirstOverlap(addrs).isDefined
+}
